2f754dbadcf80fcbebca28bd641d4e58cdc0d0e5
Felipe <afaserna@gmail.com>
2024-08-16 18:05:46 -0500

    Se queman datosen un arreglo condos objetos para el inicio de sesion

---
+++ model/ModelUsauario.js
@@ -0,0 +1,14 @@
+let usuarios = [
+    {
+      usuario: "FELIPE",
+      contrasena: "123456",
+      correo: "afa@gmail.com",
+    },
+    {
+      usuario: "USUARIO",
+      contrasena: "123456",
+      correo: "usu@gmail.com",
+    },
+  ];
+  
+  export default usuarios;
